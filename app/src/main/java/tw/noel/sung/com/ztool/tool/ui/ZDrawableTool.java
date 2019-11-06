1536e2fb1228c926367f9ca80331963157ac1c02
noel_yulon_energe <noel.sung@yulon-energy.com.tw>
2019-11-06 15:26:23 +0800

    #ZDrawableTool添加漸層相關功能

--- app/src/main/java/tw/noel/sung/com/ztool/tool/ui/ZDrawableTool.java
+++ app/src/main/java/tw/noel/sung/com/ztool/tool/ui/ZDrawableTool.java
@@ -76,8 +76,107 @@ public class ZDrawableTool extends GradientDrawable {
     /***
      *  透明度
      */
-    public ZDrawableTool alpha(int value){
+    public ZDrawableTool alpha(int value) {
         setAlpha(value);
         return this;
+    }
+
+    //---------------------
+
+    /***
+     *  設置漸層
+     */
+    public ZGradientDrawableBuilder buildGradientDrawable() {
+        return new ZGradientDrawableBuilder();
+    }
+
+
+    //----------------
+
+    /***
+     *  漸層相關類
+     */
+    public class ZGradientDrawableBuilder {
+        //正上方至正下方
+        private final int TOP_TO_BOTTOM = 1;
+        //右上至左下
+        private final int TOP_RIGHT_TO_BOTTOM_LEFT = 2;
+        //正右方至正左方
+        private final int RIGHT_TO_LEFT = 3;
+        //右下方至左上方
+        private final int BOTTOM_RIGHT_TO_TOP_LEFT = 4;
+        //正下方至正上方
+        private final int BOTTOM_TO_TOP = 5;
+        //左下方至右上方
+        private final int BOTTOM_LEFT_TO_TOP_RIGHT = 6;
+        //正左方至正右方
+        private final int LEFT_TO_RIGHT = 7;
+        //左上方至右下方
+        private final int TOP_LEFT_TO_BOTTOM_RIGHT = 8;
+
+
+        /***
+         *  設置漸層色
+         * @param colors
+         * @return
+         */
+        public ZGradientDrawableBuilder setGradientColors(int[] colors) {
+            ZDrawableTool.this.setColors(colors);
+            return this;
+        }
+
+        //--------------------
+
+        /***
+         *  設置漸變方向
+         * @param orientation
+         * @return
+         */
+        public ZGradientDrawableBuilder setGradientFromTo(int orientation) {
+            ZDrawableTool.this.setOrientation(getOrientation(orientation));
+            return this;
+        }
+
+        //--------------------
+
+        /***
+         * 設置漸變中心
+         * @param x
+         * @param y
+         * @return
+         */
+        public ZGradientDrawableBuilder setGradientCenter(float x, float y) {
+            ZDrawableTool.this.setGradientCenter(x, y);
+            return this;
+        }
+
+        //--------------------
+
+        /***
+         * 取得對應enum
+         * @param orientation
+         * @return
+         */
+        private GradientDrawable.Orientation getOrientation(int orientation) {
+            switch (orientation) {
+                case TOP_TO_BOTTOM:
+                    return Orientation.TOP_BOTTOM;
+                case TOP_RIGHT_TO_BOTTOM_LEFT:
+                    return Orientation.TR_BL;
+                case RIGHT_TO_LEFT:
+                    return Orientation.RIGHT_LEFT;
+                case BOTTOM_RIGHT_TO_TOP_LEFT:
+                    return Orientation.BR_TL;
+                case BOTTOM_TO_TOP:
+                    return Orientation.BOTTOM_TOP;
+                case BOTTOM_LEFT_TO_TOP_RIGHT:
+                    return Orientation.BL_TR;
+                case TOP_LEFT_TO_BOTTOM_RIGHT:
+                    return Orientation.TL_BR;
+                default:
+                case LEFT_TO_RIGHT:
+                    return Orientation.LEFT_RIGHT;
+            }
+        }
     }
 }
