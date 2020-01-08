37106e2e82eb245bada09a8aab8abbbcf7b2ab98
noel_yulon_energe <noel.sung@yulon-energy.com.tw>
2020-01-08 13:35:15 +0800

    clean code

--- app/src/main/java/tw/noel/sung/com/ztool/connect/z_connect/util/dialog/ZLoadingDialog.java
+++ app/src/main/java/tw/noel/sung/com/ztool/connect/z_connect/util/dialog/ZLoadingDialog.java
@@ -1,28 +1,13 @@
 package tw.noel.sung.com.ztool.connect.z_connect.util.dialog;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptIntrinsicBlur;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-
-import tw.noel.sung.com.ztool.R;
 import tw.noel.sung.com.ztool.connect.z_connect.util.views.ZLoadingView;
 import tw.noel.sung.com.ztool.tool.ZCheckDeviceTool;
 
@@ -34,23 +19,23 @@ public class ZLoadingDialog extends Dialog implements DialogInterface.OnShowList
 
     private LinearLayout layout;
     private ZLoadingView zLoadingView;
-    private Activity activity;
+    private Context context;
     private ZCheckDeviceTool zCheckDeviceTool;
     private int phoneWidth;
     private int phoneHeight;
 
     public ZLoadingDialog(Context context) {
         super(context);
-        activity = (Activity) context;
-        zCheckDeviceTool = new ZCheckDeviceTool(activity);
+        this.context = context;
+        zCheckDeviceTool = new ZCheckDeviceTool(context);
         int[] phoneSize = zCheckDeviceTool.getPhoneSize();
         phoneWidth = phoneSize[0];
         phoneHeight = phoneSize[1];
 
-        layout = new LinearLayout(activity);
+        layout = new LinearLayout(context);
         layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         layout.setGravity(Gravity.CENTER);
-        zLoadingView = new ZLoadingView(activity);
+        zLoadingView = new ZLoadingView(context);
         layout.addView(zLoadingView);
 
         setContentView(layout);
